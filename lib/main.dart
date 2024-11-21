7956daf25b7240f1e8fa1f3a97bf91e7b916d4cf
mohamed321321 <mohamedhisham342000@gmail.com>
2024-11-21 14:24:23 +0200

    Connecting The Project To Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:chat_room/firebase_options.dart';
 import 'package:chat_room/screens/login_screen.dart';
 import 'package:chat_room/screens/registration_screen.dart';
 import 'package:chat_room/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  // Firebase Connection
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,6 +22,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Test Firebase Connection
+      home: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Text("Done");
+          }
+          return Text("Loading");
+        },
+      ),
       initialRoute: WelcomeScreen.id,
       routes: {
         //WelcomeScreen Route
